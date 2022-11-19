41f131c3c6fa4135acaa1fee95fa53fee9d077cb
Pheon-Dev <devpheon@gmail.com>
2022-11-19 15:14:42 +0300

    edit: colours

--- colors/dracula.h
+++ colors/dracula.h
@@ -1,9 +1,9 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#7f7f7f", "#363636" },
+	[SchemeNorm] = { "#7f7f7f", "#3f3f3f" },
 	[SchemeSel] = { "#c0caf5", "#2e2e2e" },
 	[SchemeSelHighlight] = { "#7aa2f7", "#2e2e2e" },
-	[SchemeNormHighlight] = { "#ff8fff", "#363636" },
-	[SchemeOut] = { "#363636", "#9ece6a" },
-	[SchemeMid] = { "#7f7f7f", "#363636" },
+	[SchemeNormHighlight] = { "#ff8fff", "#3f3f3f" },
+	[SchemeOut] = { "#3f3f3f", "#9ece6a" },
+	[SchemeMid] = { "#7f7f7f", "#3f3f3f" },
 };
