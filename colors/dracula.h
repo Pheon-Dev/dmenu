226cf5068a6b3913a53d3676ff3c20c9f9d2f5fd
Pheon-Dev <devpheon@gmail.com>
2023-06-14 19:17:52 +0300

    [+] dmenu stable config

---
+++ colors/dracula.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#a9a1e1", "#282a36" },
+	[SchemeSel] = { "#c0caf5", "#21222c" },
+	[SchemeSelHighlight] = { "#bd93f9", "#21222c" },
+	[SchemeNormHighlight] = { "#7aa2f7", "#282a36" },
+	[SchemeOut] = { "#282a36", "#50fa7b" },
+	[SchemeMid] = { "#a9a1e1", "#282a36" },
+};
